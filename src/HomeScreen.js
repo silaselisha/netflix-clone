a65d98adaea7b58d0da3a6b845f8bce0498b9f31
Elisha K. Silas <elisilas87@outlook.com>
2022-08-15 16:50:03 +0300

    Navigation section

---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav'
+
+const HomeScreen = () => {
+  return (
+    <div className='homeScreen'>
+        {/* Nav */}
+        <Nav />
+        {/* Banner */}
+        {/* Row */}
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDPJt2X6gAcIsy_UTWwafIo_JtfEXwMH5E",
+    authDomain: "netflix-adae4.firebaseapp.com",
+    projectId: "netflix-adae4",
+    storageBucket: "netflix-adae4.appspot.com",
+    messagingSenderId: "542644492739",
+    appId: "1:542644492739:web:01c394eb42232c7b578576",
+    measurementId: "G-40XY3P4LJP"
+};
