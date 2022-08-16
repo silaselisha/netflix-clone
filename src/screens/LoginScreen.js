de077f8360d238cdf3914e93990cfbf32e30569e
Elisha K. Silas <elisilas87@outlook.com>
2022-08-16 23:05:07 +0300

    Signup form

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginScreen.css'
+import SignupScreen from './SignupScreen'
 
 const LoginScreen = () => {
+  const [signIn, setSignIn] = useState(false)
+
   return (
     <div className='loginScreen'>
         <div className='loginScreen__background'>
             <img className='loginScreen__logo' src={`https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png`} alt='netflix logo' />
              
-            <button className='loginScreen__button'>Sign In</button>
+            <button className='loginScreen__button' onClick={() => setSignIn(true)}>Sign In</button>
             <div className='loginScreen__gradient' />
         </div>
         <div className='loginScreen__body'>
-            <>
-                <h1>Unlimited movies, TV shows, and more.</h1>
-                <h2>Watch anywhere. Cancel anytime.</h2>
-                <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-                <div className='loginScreen__input'>
-                    <form>
-                        <input type='email' placeholder='Email address' />
-                        <button type='button' className='loginScreen__getStarted'>Get Started</button>
-                    </form>
-                </div>
-            </>
+            {signIn ? <SignupScreen /> : (
+                <>
+                    <h1>Unlimited movies, TV shows, and more.</h1>
+                    <h2>Watch anywhere. Cancel anytime.</h2>
+                    <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                    <div className='loginScreen__input'>
+                        <form>
+                            <input type='email' placeholder='Email address' />
+                            <button type='button' className='loginScreen__getStarted' onClick={() => setSignIn(true)}>Get Started</button>
+                        </form>
+                    </div>
+                </>
+            )}
         </div>
     </div>
   )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './SignupScreen.css'
 
 const SignupScreen = () => {
   return (
     <div className='signupScreen'>
-        SignupScreen
+       <form>
+          <h1>Sign In</h1>
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+          <button type='submit'>Sign In</button>
+
+          <h4>
+            <span className='signupScreen__gray'>New to Netflix?  </span>
+            <span className='signupScreen__link'>Sign Up now.</span>
+          </h4>
+       </form>
     </div>
   )
 }
